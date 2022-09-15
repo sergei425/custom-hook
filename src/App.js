483eb42e2ed806242ac20de6650b18ab7b203bc9
sergei425 <105748138+sergei425@users.noreply.github.com>
2022-09-15 20:50:15 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,9 +2,9 @@ import useJsonFetch from './hooks/useJsonFetch';
 import './App.css';
 
 function App() {
-  const {data} = useJsonFetch('http://localhost:7070/', 'data')
-  const {error} = useJsonFetch('http://localhost:7070/', 'error')
-  const {loading} = useJsonFetch('http://localhost:7070/', 'loading')
+  useJsonFetch('http://localhost:7070/', 'data')
+  useJsonFetch('http://localhost:7070/', 'error')
+  useJsonFetch('http://localhost:7070/', 'loading')
   return (
     <div className="App">
       hi
